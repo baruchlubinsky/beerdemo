bffbcd8ec62c548471bfea7d909735959c7c9efa
Baruch Lubinsky <baruchlubinsky@gmail.com>
2014-07-29 21:24:58 +0200

    editting

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ var Router = Ember.Router.extend({
 Router.map(function() {
 	this.resource('beers', function() {
     	this.route('new');
+    	this.route('edit', {path: ':id/edit'});
   	});
 });
 
